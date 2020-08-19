ec495837f3bcd4adbaaef8839da342935513d8bf
jihane88 <jihanemaftouh88@gmail.com>
2020-08-19 14:29:35 +0200

    Revert "update backend"
    This reverts commit b2385715

--- backend/src/main/java/com/akveo/bundlejava/config/EsConfig.java
+++ backend/src/main/java/com/akveo/bundlejava/config/EsConfig.java
@@ -1,59 +1,60 @@
-//package com.akveo.bundlejava.config;
+package com.akveo.bundlejava.config;
+
+import org.elasticsearch.client.Client;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.TransportAddress;
+import org.elasticsearch.transport.client.PreBuiltTransportClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
+import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+
+import java.net.InetAddress;
+
+//http://docs.spring.io/spring-boot/docs/current/reference/html/boot-features-nosql.html#boot-features-connecting-to-elasticsearch-spring-data
+//https://github.com/spring-projects/spring-boot/tree/master/spring-boot-samples/spring-boot-sample-data-elasticsearch/src/main/java/sample/data/elasticsearch
+//http://docs.spring.io/spring-data/elasticsearch/docs/current/reference/html/#elasticsearch.repositories
+//http://geekabyte.blogspot.my/2015/08/embedding-elasticsearch-in-spring.html
+//https://github.com/spring-projects/spring-data-elasticsearch/wiki/Spring-Data-Elasticsearch---Spring-Boot---version-matrix
+@Configuration
+@EnableElasticsearchRepositories(basePackages = "com.akveo.bundlejava.search")
+public class EsConfig {
+
+//    @Value("${elasticsearch.host}")
+//    private String EsHost;
 //
-//import org.elasticsearch.client.Client;
-//import org.elasticsearch.client.transport.TransportClient;
-//import org.elasticsearch.common.settings.Settings;
-//import org.elasticsearch.common.transport.TransportAddress;
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.context.annotation.Lazy;
-//import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
-//import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
-//import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+//    @Value("${elasticsearch.port}")
+//    private int EsPort;
 //
-//import java.net.InetAddress;
-//
-////http://docs.spring.io/spring-boot/docs/current/reference/html/boot-features-nosql.html#boot-features-connecting-to-elasticsearch-spring-data
-////https://github.com/spring-projects/spring-boot/tree/master/spring-boot-samples/spring-boot-sample-data-elasticsearch/src/main/java/sample/data/elasticsearch
-////http://docs.spring.io/spring-data/elasticsearch/docs/current/reference/html/#elasticsearch.repositories
-////http://geekabyte.blogspot.my/2015/08/embedding-elasticsearch-in-spring.html
-////https://github.com/spring-projects/spring-data-elasticsearch/wiki/Spring-Data-Elasticsearch---Spring-Boot---version-matrix
-//@Configuration
-//@EnableElasticsearchRepositories(basePackages = "com.akveo.bundlejava.search")
-//public class EsConfig {
-//
-////    @Value("${elasticsearch.host}")
-////    private String EsHost;
-////
-////    @Value("${elasticsearch.port}")
-////    private int EsPort;
-////
-////    @Value("${elasticsearch.clustername}")
-////    private String EsClusterName;
-//
-////    @Bean
-////    public Client client() throws Exception {
-////
-////        Settings esSettings = Settings.builder()
-////                .put("cluster.name", "docker-cluster")
-////                .build();
-////
-////        //https://www.elastic.co/guide/en/elasticsearch/guide/current/_transport_client_versus_node_client.html
-////        TransportClient client = new PreBuiltTransportClient(esSettings)
-////                .addTransportAddress(new TransportAddress(InetAddress.getByName("163.172.100.227"), 9300));
-////        return client;
-////    }
-//
-////    @Bean
-////    public ElasticsearchOperations elasticsearchTemplate() throws Exception {
-////        return new ElasticsearchTemplate(client());
-////    }
-//
-//    //Embedded Elasticsearch Server
-////    @Bean
-////    public ElasticsearchOperations elasticsearchTemplate() {
-////        return new ElasticsearchTemplate(nodeBuilder().local(true).node().client());
-////    }
-//
-//}
+//    @Value("${elasticsearch.clustername}")
+//    private String EsClusterName;
+
+    @Bean
+    public Client client() throws Exception {
+
+        Settings esSettings = Settings.builder()
+                .put("cluster.name", "docker-cluster")
+                .build();
+
+        //https://www.elastic.co/guide/en/elasticsearch/guide/current/_transport_client_versus_node_client.html
+        TransportClient client = new PreBuiltTransportClient(esSettings)
+                .addTransportAddress(new TransportAddress(InetAddress.getByName("163.172.100.227"), 9300));
+        return client;
+    }
+
+//    @Bean
+//    public ElasticsearchOperations elasticsearchTemplate() throws Exception {
+//        return new ElasticsearchTemplate(client());
+//    }
+
+    //Embedded Elasticsearch Server
+//    @Bean
+//    public ElasticsearchOperations elasticsearchTemplate() {
+//        return new ElasticsearchTemplate(nodeBuilder().local(true).node().client());
+//    }
+
+}
--- backend/src/main/java/com/akveo/bundlejava/index/File.java
+++ backend/src/main/java/com/akveo/bundlejava/index/File.java
@@ -1,46 +1,10 @@
-package com.akveo.bundlejava.index;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.springframework.data.elasticsearch.annotations.DateFormat;
-import org.springframework.data.elasticsearch.annotations.Field;
-import org.springframework.data.elasticsearch.annotations.FieldType;
-
-import java.time.OffsetDateTime;
-
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
-public class File {
-
-    @Field(type = FieldType.Keyword)
-    private String extension;
-
-    @Field(type = FieldType.Keyword,name = "content_type")
-    private String contentType;
-
-    @Field(type = FieldType.Date,format = DateFormat.date_optional_time)
-    private OffsetDateTime created;
-
-    @Field(type = FieldType.Date,format = DateFormat.date_optional_time,name = "last_modified")
-    private OffsetDateTime lastModified;
-
-    @Field(type = FieldType.Date,format = DateFormat.date_optional_time,name ="last_accessed" )
-    private OffsetDateTime lastAccessed;
-
-    @Field(type = FieldType.Date,format = DateFormat.date_optional_time,name = "indexing_date")
-    private OffsetDateTime indexingDate;
-
-    @Field(type = FieldType.Long)
-    private long filesize;
-
-    @Field(type=FieldType.Keyword)
-    private String filename;
-
-    @Field(type = FieldType.Keyword)
-    private String url;
-
-}
+//package com.akveo.bundlejava.index;
+//
+//import org.springframework.data.annotation.Id;
+//import org.springframework.data.elasticsearch.annotations.Document;
+//
+//@Document(indexName = "files_index", indexStoreType = "fs")
+//public class File {
+//    @Id
+//    private String string;
+//}
